4f25b9a9205ea7adba4a87a07ad0f87ddbc05b4a
Avi Garg <a7vi1912@gmail.com>
2018-04-07 02:16:55 +0530

    Add files via upload

--- lowpassfreq1.py
+++ lowpassfreq1.py
@@ -15,6 +15,7 @@ class low_pass:
     def __init__(self, master):
         self.frame1 = Frame(master)
         self.frame2 = Frame(master)
+        self.frame3 = Frame(master)
         hbtn = Button(self.frame2, text="OPEN IMAGE", command=lambda: self.button_click(master))
         hbtn.pack(fill="none", expand=True)
         self.initUI(master)
@@ -22,6 +23,7 @@ class low_pass:
     def initUI(self, master):
         self.frame1.grid(row=0, column=0)
         self.frame2.grid(row=10, column=0)
+        self.frame3.grid(row=0, column =9)
 
         if len(low_pass.path) > 0:
             img = cv2.imread(low_pass.path, 0)
@@ -60,7 +62,7 @@ class low_pass:
             magnitude_spectrum = 20 * np.log(np.abs(fshift))
 
             fig = Figure(figsize=(8, 8))
-            fig.suptitle("Ideal lowpass_filters")
+            fig.suptitle("Ideal lowpass filters- With Ringing Effect")
             a = fig.add_subplot(221)
             a.set_title("Original Image")
             a.imshow(img, cmap='gray')
@@ -74,7 +76,32 @@ class low_pass:
             d.set_title("Fourier Transformation")
             d.imshow(magnitude_spectrum, cmap='gray')
 
-            canvas = FigureCanvasTkAgg(fig, master)
+            canvas = FigureCanvasTkAgg(fig, self.frame1)
+            canvas.get_tk_widget().grid(row=1, column=0, columnspan=4, rowspan=8)
+            canvas.draw()
+
+            H3 = 1 / (1 +(D/cutoff[1])**2)
+            G3 = H3 * fourier_image
+            imback2 = np.fft.ifft2(G3)
+            imback2 = np.uint8(np.real(imback2))
+            imback2[imback2 >= 225] = 0
+
+            H4 = 1 / (1 + (D / cutoff[1]) ** 4)
+            G4 = H4 * fourier_image
+            imback3 = np.fft.ifft2(G4)
+            imback3 = np.uint8(np.real(imback3))
+            imback3[imback3 >= 225] = 0
+
+            fig1 = Figure(figsize=(8, 8))
+            fig1.suptitle("Butterworth lowpass Filter - No Ringing Effect")
+            e = fig1.add_subplot(221)
+            e.set_title("Order = 1")
+            e.imshow(imback2, cmap='gray')
+            f = fig1.add_subplot(222)
+            f.set_title("Order = 2")
+            f.imshow(imback3, cmap='gray')
+
+            canvas = FigureCanvasTkAgg(fig1, self.frame3)
             canvas.get_tk_widget().grid(row=1, column=0, columnspan=4, rowspan=8)
             canvas.draw()
 
